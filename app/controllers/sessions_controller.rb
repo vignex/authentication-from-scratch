9cc3b814b71ab1a70de2a8e4e2740d0ea5d72eea
Sudharshan <sudhar.ti@gmail.com>
2014-01-30 01:45:42 +0530

    Generated sessions controller and added route

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+ def new
+  end
+  
+  def create
+	#Call authenticate_user method
+    user = User.authenticate_user(params[:email], params[:password])
+    
+	#If user is authenticated store user.id in a session variable and redirect, Otherwise display flash message and render 'new'
+    if user
+      session[:user] = user.id
+      redirect_to root_url
+    else
+      flash.now.alert = "Invalid Email or Password"
+      render 'new'
+  end
+  
+  def destroy
+    session[:user] = nil
+    redirect_to root_url, notice: "Signed out successfully"
+  end
+end
