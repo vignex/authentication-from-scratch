411c7650e264717f06408280290b92c711569c78
Sudharshan <sudhar.ti@gmail.com>
2014-04-04 00:17:40 +0530

    Added sessions helper and included

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,37 @@
 module SessionsHelper
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    if session[:user]
+      @current_user ||= User.find(session[:user])
+    elsif cookies[:remember_token]
+      @current_user ||= User.find_by_remember_token!(cookies[:remember_token])
+    end
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+
+  def sign_in(user)
+    cookies.permanent[:remember_token] = user.remember_token
+    self.current_user = user
+  end
+
+  def sign_out
+    self.current_user = nil
+    cookies.delete(:remember_token)
+  end
+
+  def session_create(user_id)
+    session[:user] = user_id
+  end
+
+  def session_destroy
+    session[:user] = nil
+  end
+
 end
