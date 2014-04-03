c403c88da309b371fef5f61afa4a92db5829a693
Sudharshan <sudhar.ti@gmail.com>
2014-04-04 00:30:47 +0530

    Mailer config for dev and production

--- config/environment.rb
+++ config/environment.rb
@@ -3,16 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 AuthenticationFromScratch::Application.initialize!
-
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.perform_deliveries = true
-
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => "587",
-  :domain               => "gmail.com",
-  :user_name            => ENV["USERNAME"],
-  :password             => ENV["PASSWORD"],
-  :authentication       => "plain",
-  :enable_starttls_auto => true  
-}
