f4a862dc656624948221e3adcc9d0d1d329be170
Sudharshan <sudhar.ti@gmail.com>
2014-11-09 01:53:26 -0600

    Layout changes, Appplied bootstrap to all pages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,8 @@ class SessionsController < ApplicationController
 
   before_action :check_signed_in, except: :destroy
 
-  def new
-  end
-
   def create
     user = User.authenticate_user(params[:email], params[:password])
-
     if user
       sign_in user if params[:remember_me]
       session_create user.id
